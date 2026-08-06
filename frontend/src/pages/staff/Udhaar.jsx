79c1b879ae260ca14fc1f58a2c0bd5456f839a3e
Bhoomika Sindhu <51417711924_aids@vipstc.edu.in>
2026-08-06 12:24:27 +0530

    changed udhaar system

--- frontend/src/pages/staff/Udhaar.jsx
+++ frontend/src/pages/staff/Udhaar.jsx
@@ -51,17 +51,20 @@ export default function Udhaar() {
     }
   }
 
-  async function addUdhaar(data) {
-    try {
-      await udhaarService.createUdhaar(data);
+  async function addUdhaar(entries) {
+  try {
+    await udhaarService.createUdhaar(
+      reportId,
+      entries
+    );
 
-      setShowAdd(false);
+    setShowAdd(false);
 
-      await loadPage();
-    } catch (err) {
-      console.error(err);
-    }
+    await loadPage();
+  } catch (err) {
+    console.error(err);
   }
+}
 
   async function repay(id, amount) {
     try {
